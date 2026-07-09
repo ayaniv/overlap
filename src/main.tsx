54721ea8aafd8104643b972b2947437510da526b
Yaniv Aharon <yaniv@honeybook.com>
2026-07-09 14:51:06 +0300

    refactor: wire AnalyticsProvider in main.tsx and drop @posthog/react

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import posthog from 'posthog-js'
-import { PostHogProvider } from '@posthog/react'
-
-posthog.init(import.meta.env.VITE_POSTHOG_PROJECT_TOKEN, {
-  api_host: import.meta.env.VITE_POSTHOG_HOST,
-  defaults: '2026-05-30',
-})
+import { AnalyticsProvider } from './analytics/AnalyticsProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PostHogProvider client={posthog}>
+    <AnalyticsProvider>
       <App />
-    </PostHogProvider>
+    </AnalyticsProvider>
   </StrictMode>,
 )
